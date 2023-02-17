4d29a543d72058d44cf762c165c0bdd5c1cf58a9
BillionaireSteve <stevedrobe@gmail.com>
2023-02-17 13:05:55 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 /**
- * main - Prints all possible combinations of two different digits.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int digit1, digit2;
-for (digit1 = 0; digit1 < 9; digit1++)
-{
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-{
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
-if (digit1 == 8 && digit2 == 9)
-continue;
-putchar(',');
-putchar(' ');
-}
-}
+int ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
 putchar('\n');
 return (0);
 }
