c4105c2975659a3c861e205db6a0e982a4727117
BillionaireSteve <stevedrobe@gmail.com>
2023-02-21 12:50:49 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,8 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+print_alphabet();
+return (0);
 }
