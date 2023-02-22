bf138e98c6e9773721076b819797d3439b6d39e4
BillionaireSteve <stevedrobe@gmail.com>
2023-02-21 17:29:41 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * Discription- print lowercase letters x10 times
+ * print_alphabet_x10 times the alphabet in lowercase
+ * followed bay a new line
+ */
+void print_alphabet_10x(void)
+{
+char c;
+int i = 0;
+while (i <= 10)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
