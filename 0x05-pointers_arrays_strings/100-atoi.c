19f9ddb2d9b570ca0c6fe9c2bb0025fa787f9b9d
BillionaireSteve <stevedrobe@gmail.com>
2023-02-27 21:43:19 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,25 +7,25 @@
  */
 int _atoi(char *s)
 {
-unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
+unsigned int count = 0, size = 0, a = 0, b = 1, c = 1, i;
 while (*(s + count) != '\0')
 {
 if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 break;
 if (*(s + count) == '-')
-k *= -1;
+b *= -1;
 if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 {
 if (size > 0)
-m *= 10;
+c *= 10;
 size++;
 }
 count++;
 }
 for (i = count - size; i < count; i++)
 {
-j = j + ((*(s + i) - 48) * m);
-m /= 10;
+a = a + ((*(s + i) - 48) * c);
+c /= 10;
 }
-return (j * k);
+return (a * b);
 }
