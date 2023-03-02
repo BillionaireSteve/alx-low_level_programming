cf71889e81a610c20460c797959214b6a5564111
BillionaireSteve <stevedrobe@gmail.com>
2023-03-02 13:02:10 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strncpy - function
+ * @dest: destination of string
+ * @src: source of string
+ * @n: interger
+ * Return: to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int x;
+for (x = 0; x < n && src[x] != '\0'; x++)
+{
+dest[x] = src[x];
+}
+while (x < n)
+{
+dest[x] = '\0';
+x++;
+}
+return (dest);
+}
