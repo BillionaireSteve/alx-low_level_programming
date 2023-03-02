fc965175233f4e1b5cfe4b6fe61f8e7f43d4397a
BillionaireSteve <stevedrobe@gmail.com>
2023-03-02 13:45:13 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @str: the string to change
+ *
+ * Return: the string that change for uppercase
+ */
+char *string_toupper(char *str)
+{
+int y;
+y = 0;
+while (str[i] != '\0')
+{
+if (str[y] >= 97 && str[y] <= 122)
+{
+str[y] = str[y] - 32;
+}
+y++;
+}
+return (str);
+}
