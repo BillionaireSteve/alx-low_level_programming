a12b584bd70722fedaf12472a4f9f56e11b34a81
BillionaireSteve <stevedrobe@gmail.com>
2023-03-02 09:19:58 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcat - function
+ * @dest: destination of string
+ * @src: parent string
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+char *a = dest;
+while (*a != '\0')
+{
+a++;
+}
+while (*src != '\0')
+{
+*a = *src;
+a++;
+src++;
+}
+*a = '\0';
+return (dest);
+}
