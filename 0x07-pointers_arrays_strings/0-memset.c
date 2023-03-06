880a53b61833e9cdce0e148408b7a7dcb9b7d7f0
BillionaireSteve <stevedrobe@gmail.com>
2023-03-05 16:57:49 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,10 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int a = 0;
-for (; n > 0; a++)
+unsigned int a;
+for (a = 0; a < n; a++)
 {
-s[a] = b;
-a--;
+*(s + a) = b;
 }
 return (s);
 }
