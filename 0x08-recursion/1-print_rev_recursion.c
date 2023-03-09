f2fa27b3df93f66f0a17ab1ef30e0e9baf0b626e
BillionaireSteve <stevedrobe@gmail.com>
2023-03-08 17:09:01 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function
+ * @s: the string to be reversed
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+for (i = 0; str[s] ! ='\0'; s++);
+return (0);
+}
